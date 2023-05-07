dcef9f0ced7ff664fc1e1179b6204652d96abed0
jitenderji1138 <131745277+jitenderji1138@users.noreply.github.com>
2023-05-07 13:14:37 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyCr89tryLCDib-KOC4e6aleWni8iBdxfTo",
-    authDomain: "chat-application-8ff7d.firebaseapp.com",
-    projectId: "chat-application-8ff7d",
-    storageBucket: "chat-application-8ff7d.appspot.com",
-    messagingSenderId: "1000522788422",
-    appId: "1:1000522788422:web:6db8ac69aaf60d5439e447",
-    measurementId: "G-P0CQLB2403"
+     apiKey: "AIzaSyDNQ4HEpw1UI6hTD7QWXCg6kCbcvR2VMxE",
+  authDomain: "guvy-c5705.firebaseapp.com",
+  projectId: "guvy-c5705",
+  storageBucket: "guvy-c5705.appspot.com",
+  messagingSenderId: "15160196695",
+  appId: "1:15160196695:web:5bdbaf4976243e19c2ad12",
+  measurementId: "G-XZ0GVWS1FD"
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
